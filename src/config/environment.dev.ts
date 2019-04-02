28af1ad27d2a2dba2dcbe47a2857dcfec3cbda0e
Chuzzvimar <guiomarbermudez@vms.com.ve>
2019-04-02 17:06:50 -0400

    starting

--- src/config/environment.dev.ts
+++ src/config/environment.dev.ts
@@ -1,15 +1,15 @@
-/* export const ENV = {  
+ export const ENV = {  
     API_URL: 'https://chuzzstaging.skygeario.com',
     API_KEY: '3af1de5a891b4320b77a0fc8acb03810',
     APP_ID: 'com.oursky.chuzz',
     SENDER_ID: '711386408610',
-  };  */
-
+  };  
+/*
 export const ENV = {  
     API_URL: 'https://chuzz.skygeario.com',
     API_KEY: '0d3b7b0cd7a74e3cbbfe3c7ae37017c4',
     APP_ID: 'app.chuzzapp.com',
     SENDER_ID: '198515848001',
 }; 
-
+*/
   
