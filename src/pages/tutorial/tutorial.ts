477992244ae073fa6bb35a94e63625e5c5c3a122
Chuzzvimar <guiomarbermudez@vms.com.ve>
2019-04-03 00:55:28 -0400

    tutorial and translations

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -23,35 +23,76 @@ export class TutorialPage {
   slidesMoving;
   SwipedTabsSlider;
   slidesHeight;
+  TUTO_SLIDE1_TITLE = "";
+  TUTO_SLIDE2_TITLE = "";
+  TUTO_SLIDE3_TITLE = "";
+  TUTO_SLIDE1_DES = "";
+  TUTO_SLIDE2_OPC = "";
+  TUTO_SLIDE2_OPC1 = "";
+  TUTO_SLIDE2_OPC2 = "";
+  TUTO_SLIDE3_MENUT = "";
+  TUTO_SLIDE3_MENUD = "";
+  TUTO_SLIDE3_PERFILT = "";
+  TUTO_SLIDE3_PERFILD = "";
+  TUTO_SLIDE4_TITLE = "";
+  TUTO_SLIDE4_BUSCART = "";
+  TUTO_SLIDE4_BUSCARD = "";
+  TUTO_SLIDE4_FOLLOWT = "";
+  TUTO_SLIDE4_FOLLOWD = "";
+  TITLE_DELETE_POLL = "";
 
-  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService) {
-    translate.get(["TUTORIAL_SLIDE1_TITLE",
-      "TUTORIAL_SLIDE1_DESCRIPTION",
-      "TUTORIAL_SLIDE2_TITLE",
-      "TUTORIAL_SLIDE2_DESCRIPTION",
-      "TUTORIAL_SLIDE3_TITLE",
-      "TUTORIAL_SLIDE3_DESCRIPTION",
-    ]).subscribe(
-      (values) => {
+  constructor(public navCtrl: NavController, public menu: MenuController, public translate: TranslateService) {
+  
+      this.translate.get([
+        'TUTORIAL_SLIDE1_TITLE',
+      'TUTORIAL_SLIDE1_DESCRIPTION',
+      'TUTORIAL_SLIDE2_TITLE',
+      'TUTORIAL_SLIDE2_DESCRIPTION',
+      'TUTORIAL_SLIDE3_TITLE',
+      'TUTORIAL_SLIDE3_DESCRIPTION',
+      'TUTO_SLIDE1_TITLE',
+      'TUTO_SLIDE1_DES',
+      'TUTO_SLIDE2_TITLE',
+      'TUTO_SLIDE2_OPC',
+      'TUTO_SLIDE2_OPC1',
+      'TUTO_SLIDE2_OPC2',
+      'TUTO_SLIDE3_TITLE',
+      'TUTO_SLIDE3_MENUT',
+      'TUTO_SLIDE3_MENUD',
+      'TUTO_SLIDE3_PERFILT',
+      'TUTO_SLIDE3_PERFILD',
+      'TUTO_SLIDE4_TITLE',
+      'TUTO_SLIDE4_BUSCART',
+      'TUTO_SLIDE4_BUSCARD',
+      'TUTO_SLIDE4_FOLLOWT',
+      'TUTO_SLIDE4_FOLLOWD',
+      'TITLE_DELETE_POLL',]).subscribe(values => {
         console.log('Loaded values', values);
-        this.slides = [
-          {
-            title: "What is chuzz?",
-            description: "Description of chuzz",
-            image: 'assets/img/1.jpg',
-          },
-          {
-            title: "How to select categories?",
-            description: "How to select categories?",
-            image: 'assets/img/2.jpg',
-          },
-          {
-            title: "How to select country?",
-            description: "How to select country?",
-            image: 'assets/img/3.jpg',
-          }
-        ];
+  
+  
+        this.TUTO_SLIDE1_TITLE = values['TUTO_SLIDE1_TITLE'];
+        this.TUTO_SLIDE2_TITLE = values['TUTO_SLIDE2_TITLE'];
+        this.TUTO_SLIDE3_TITLE = values['TUTO_SLIDE3_TITLE'];
+        this.TUTO_SLIDE1_DES = values['TUTO_SLIDE1_DES'];
+        this.TUTO_SLIDE2_OPC = values['TUTO_SLIDE2_OPC'];
+        this.TUTO_SLIDE2_OPC1 = values['TUTO_SLIDE2_OPC1'];
+        this.TUTO_SLIDE2_OPC2 = values['TUTO_SLIDE2_OPC2'];
+        this.TUTO_SLIDE3_TITLE = values['TUTO_SLIDE3_TITLE'];
+        this.TUTO_SLIDE3_MENUT = values['TUTO_SLIDE3_MENUT'];
+        this.TUTO_SLIDE3_MENUD = values['TUTO_SLIDE3_MENUD'];
+        this.TUTO_SLIDE3_PERFILT = values['TUTO_SLIDE3_PERFILT'];
+        this.TUTO_SLIDE3_PERFILD = values['TUTO_SLIDE3_PERFILD'];
+        this.TUTO_SLIDE4_TITLE = values['TUTO_SLIDE4_TITLE'];
+        this.TUTO_SLIDE4_BUSCART = values['TUTO_SLIDE4_BUSCART'];
+        this.TUTO_SLIDE4_BUSCARD = values['TUTO_SLIDE4_BUSCARD'];
+        this.TUTO_SLIDE4_FOLLOWT = values['TUTO_SLIDE4_FOLLOWT'];
+        this.TUTO_SLIDE4_FOLLOWD = values['TUTO_SLIDE4_FOLLOWD'];
+        this.TITLE_DELETE_POLL = values['TITLE_DELETE_POLL'];
+  
+      
+  
       });
+      
   }
 
   startApp() {
@@ -74,22 +115,22 @@ export class TutorialPage {
     // enable the root left menu when leaving the tutorial page
     this.menu.enable(true);
   }
-  slideDidChange () {
-    setTimeout( () => { this.updateSliderHeight(); }, 1200); 
-   }
-   slideWillChange () {
-     this.slidesMoving = true;
-   }
-     updateSliderHeight(){
-       this.slidesMoving = false;
-       let slideIndex : number = this.SwipedTabsSlider.getActiveIndex();
-       let currentSlide : Element = this.SwipedTabsSlider._slides[slideIndex];
-       this.slidesHeight = currentSlide.clientHeight;
-   
-       console.log('index slide',slideIndex )
-       console.log('current slide',currentSlide )
-       console.log('height of slide', this.slidesHeight);
-   
-     }
+  slideDidChange() {
+    setTimeout(() => { this.updateSliderHeight(); }, 1200);
+  }
+  slideWillChange() {
+    this.slidesMoving = true;
+  }
+  updateSliderHeight() {
+    this.slidesMoving = false;
+    let slideIndex: number = this.SwipedTabsSlider.getActiveIndex();
+    let currentSlide: Element = this.SwipedTabsSlider._slides[slideIndex];
+    this.slidesHeight = currentSlide.clientHeight;
+
+    console.log('index slide', slideIndex)
+    console.log('current slide', currentSlide)
+    console.log('height of slide', this.slidesHeight);
+
+  }
 
 }
